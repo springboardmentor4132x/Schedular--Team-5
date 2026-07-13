6745f02f1794f32b64ca1609075804edb9bfab56
Lakshmi Devi Borusu <lavanyaborusu407@gmail.com>
2026-07-13 18:36:13 +0530

    Added frontend files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>📅 Social Media Scheduler</h1>
+      <p>Frontend setup completed successfully.</p>
+
+      <h3>Team 5</h3>
+
+      <button onClick={() => alert("Button Clicked!")}>
+  Get Started
+</button>
+
+    </div>
+  );
+}
+
+export default App;
